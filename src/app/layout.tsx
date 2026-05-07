f7cd387e6ccd65c608481f916aaa5ea88dbd51fa
Kamil Alfaris <kamilalfaris@gmail.com>
2026-05-07 11:11:10 +0700

    ui: remove floating install button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "@/components/providers";
-import { InstallPWA } from "@/components/InstallPWA";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -49,9 +48,6 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <NextAuthProvider>
           {children}
-          <div className="fixed bottom-6 right-6 z-50">
-            <InstallPWA />
-          </div>
         </NextAuthProvider>
       </body>
     </html>
